ed9ecff758845f3ef321c8fa79914bf0d58038a7
Cabavas <cabavas@gmail.com>
2023-08-12 06:57:35 -0300

    Projeto desafio poo

---
+++ src/main/java/org/example/Iphone.java
@@ -0,0 +1,48 @@
+package org.example;
+
+public class Iphone implements AparelhoTelefonico, NavegadorInternet, ReprodutorMusical{
+    @Override
+    public void ligar() {
+        System.out.println("Ligando para contato");
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Chamada atendida");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Correio de voz iniciado");
+    }
+
+    @Override
+    public void exibirPagina() {
+        System.out.println("Página exibida");
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba aberta");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Página atualizada");
+    }
+
+    @Override
+    public void tocar() {
+        System.out.println("Tocando música");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Música pausada");
+    }
+
+    @Override
+    public void selecionarMusica() {
+        System.out.println("Música selecionada");
+    }
+}
---
+++ src/main/java/org/example/NavegadorInternet.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public interface NavegadorInternet {
+    void exibirPagina();
+    void adicionarNovaAba();
+    void atualizarPagina();
+}
